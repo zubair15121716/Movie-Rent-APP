10ec71de060b504da52faccaad151306faa26546
zubair15121716 <m.zubair162004@gmail.com>
2022-09-19 19:23:53 +0500

    Movie On Rent APP

---
+++ Movie.py
@@ -0,0 +1,70 @@
+from tkinter import *
+from tkinter import font
+from tkinter.tix import ButtonBox
+import time
+from PIL import ImageTk, Image
+import datetime 
+import threading as th
+
+root = Tk()
+
+movies={'Avatar':'!rent','Avengers: Endgame':'!rent','Avengers: Infinity War':"!rent",'Spider-Man: No Way Home':"!rent",'Jurassic World':"!rent",'The Lion King':"!rent",'Furious 7':"!rent",'Black Panther':"!rent",'Minions':"!rent"}
+
+
+root.geometry('1366x768')
+root.minsize(400,400)
+title_app= Label(root,text="Movie Rent App",bg='red',fg='white',font=('batang', 20, 'bold'), relief=GROOVE)
+title_app.pack(fill=X,padx=10)
+
+img = ImageTk.PhotoImage(Image.open("e:\zgeTuV.png"))  
+l=Label(image=img)
+l.pack()
+
+f1=Frame(width=400,height=600,bg='red') 
+f1.place(x=0,y=150)
+
+f1.pack_propagate(False)
+x=0
+
+
+def check(p,m):
+    d= datetime.datetime.now()
+    print(p)
+    if(d.strftime("%M")==str(p)):
+        y1['button'+str(m)].config(text='Rent')
+        return 1
+
+
+
+def expire(m):
+    d= datetime.datetime.now()
+    if(movies[m]=='!rent'):
+        movies=='rent'
+        p=int(d.strftime("%M"))+1
+        y1['button'+str(m)].config(text=f'Rented for 1 Min')
+        check(p,m)
+        st = th.Timer(60.0,check,args=(p,m))
+        st.start()
+    else:
+        pass
+       
+y1={}
+y={}
+
+    
+for i in movies.keys():
+    
+    y1.update({'button'+str(i):(Button(f1,text='Rent',bg='black',fg='white',command= lambda m=str(i): expire(m)))})
+    y.update({'label'+str(i):Label(f1,text=str(x+1)+'-'+i,bg='red',fg='white',font=('arial',11,'bold'))})
+    
+    y['label'+str(i)].pack(pady=5,anchor='w')
+    y1['button'+str(i)].pack(padx=50,anchor='w')
+    x+=1
+
+
+headin=Label(root,text='MOVIES Avaliable for Rent',bg='red',fg='white',font=('calibari',15,'bold'),relief=SUNKEN)
+headin.place(x=0,y=130)
+
+
+
+root.mainloop()
